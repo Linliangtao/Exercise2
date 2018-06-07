7f6506bbbec6026bb2802279f27176acb3a145f2
linliangtao <1428129601@qq.com>
2018-06-07 13:44:28 +0800

    jsdemo

--- jsdemo.js
+++ jsdemo.js
@@ -9,8 +9,8 @@ process.chdir("./" + dirName)
 fs.mkdirSync('css') 
 fs.mkdirSync('js') 
 
-fs.writeFileSync("./index.html", "")
-fs.writeFileSync("css/style.css", "")
-fs.writeFileSync("./js/main.js", "")
+fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
+fs.writeFileSync("css/style.css", "h1{color: red;}")
+fs.writeFileSync("./js/main.js", "var string = "Hello World" alert(string)")
 
 process.exit(0)
